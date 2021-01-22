e029d3d6be876dffa4b869d24d4be136dfafd9c0
Nergiz Özge ERDAĞI <nergiz.erdagi@std.ankaramedipol.edu.tr>
2021-01-22 15:46:58 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer()
+            {
+                Id = 117,
+                name = "Nergiz Özge",
+                LastName = "Erdağı",
+
+
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(customer);
+            musteriManager.list(customer);
+            musteriManager.Sil(customer);
+
+            MusteriYazi musteriyazi = new MusteriYazi();
+
+            musteriyazi.Yazdir(musteriyazi);
+
+
+
+           
+
+
+        }     
+    }
+}
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string LastName { get; set; }
+        
+    }
+
+    class MusteriManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(" ( " + customer.Id + " - " + customer.name + " " + customer.LastName + " ) " + "Müşteri eklendi.");
+        }
+
+        public void list(Customer customer)
+        {
+            Console.WriteLine(customer.name + " " + customer.LastName + " " + "Listelendi.");
+        }
+
+        public void Sil(Customer customer)
+        {
+            Console.WriteLine(customer.name + " " + customer.LastName + " " + "Listeden silindi.");
+        }
+
+    }
+
+    class MusteriYazi
+    {
+        public string yazi { get; set; }
+    
+    public void Yazdir(MusteriYazi musteriyazi)
+         
+        {
+        string yazi = "İyi ve sağlıklı günler dileriz.";
+        Console.WriteLine(yazi);
+        }
+    }
+   
+
